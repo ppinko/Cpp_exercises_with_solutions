45bff8c3046c0b7756a6e64013236fa7ef0c825f
ppinko <p.pinkowicz@gmail.com>
2020-06-11 11:32:36 +0200

    Adding additional solution to the problem pf counting ones in 2d array.

--- arrays_exercises/count_ones_in_2d_array.cpp
+++ arrays_exercises/count_ones_in_2d_array.cpp
@@ -4,15 +4,27 @@ https://edabit.com/challenge/3dBpTdfBsnGYrKGxh
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+// int count_ones(std::vector< std::vector<int> > matrix) {
+//     int tot {0};
+//     int not_one {0};
+//     for (auto i: matrix){
+//         for (auto j: i)
+//             (j == 1) ? tot++ : not_one++;
+//     }
+//     return tot;
+// }
+
+// Alternative solution
 
 int count_ones(std::vector< std::vector<int> > matrix) {
-    int tot {0};
-    int not_one {0};
-    for (auto i: matrix){
-        for (auto j: i)
-            (j == 1) ? tot++ : not_one++;
-    }
-    return tot;
+	int total = 0;
+  	for (std::vector<int> t : matrix) 
+	{
+	  total += std::count(t.begin(), t.end(), 1);
+	}
+  	return total;
 }
 
 int main(){
