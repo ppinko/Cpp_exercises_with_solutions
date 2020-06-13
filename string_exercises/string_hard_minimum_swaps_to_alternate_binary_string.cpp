8127c7b76decdd33919483ac1206e7de9cbb1f7e
ppinko <p.pinkowicz@gmail.com>
2020-06-13 18:37:40 +0200

    Adding string exercise - minimum swaps to alternate binary string.

---
+++ string_exercises/string_hard_minimum_swaps_to_alternate_binary_string.cpp
@@ -0,0 +1,25 @@
+// https://edabit.com/challenge/6eKE83afJNGfQjtTF
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int minSwaps(std::string str) {
+	int start_one = 0;
+    int start_zero = 0;
+    int index = 0;
+    for (char& c: str){
+        if ((index % 2 == 0 && c == '1') || (index % 2 == 1 && c == '0')) start_zero++;
+        else start_one++;
+        index++;
+    }
+    std::cout << "ones = " << start_one << " zeros = " << start_zero << "\n";
+    return std::min(start_one, start_zero);
+}
+
+int main(){
+    minSwaps("010010111");
+    minSwaps("010101010");
+    std::cout << "Success" << std::endl;
+    return 0;
+}
