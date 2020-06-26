3a137fbb6d92a14ff37277f733e5c424323001ee
ppinko <p.pinkowicz@gmail.com>
2020-06-26 23:29:03 +0200

    Refactoring code.

---
+++ interview_questions/interview_questions_codelite/task_2_refactoring/task_2_refactoring.cpp
@@ -0,0 +1,123 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
+
+// function prototype
+int convert_comments(std::string file_name);
+
+int convert_comments(std::string file_name){
+    std::ifstream in_file;
+    in_file.open(file_name);
+    
+    // checking if file exists and was opened properly
+    if (!in_file) {
+        std::cerr << "Problem opening file" << std::endl;
+        return 1;
+    }
+    
+    // setting variables
+    std::vector<std::string> out_file {};
+    std::vector<std::string> buffer {};
+    std::string line{};
+    bool flag {false};
+    int pos_first {0}, pos_second {0};
+
+    while (std::getline(in_file, line)){
+        pos_first = line.find("/*");
+        pos_second = line.find("*/");
+        
+        // lack of starting c-style comment symbol /* and flag equals false, copy original line
+        if (!flag && pos_first == -1) 
+            out_file.push_back(line);
+        
+        // checking one-line comments
+        else if (!flag && pos_first != -1 && pos_second != -1){
+            // if closing comment symbol */ at the end of line, then convert line
+            if (pos_second + 1 == line.size() - 1){
+                line.replace(pos_first, 2, "//");
+                line.replace(pos_second, 2, "");
+                out_file.push_back(line);
+            } else {
+                // if closing comment symbol */ not at the end of line and printable
+                // objects behind closing comment symbol, then no conversion
+                flag = true;
+                for (int i = pos_second + 2; i < line.size(); i++){
+                    if (std::isprint(line[i])) {
+                        out_file.push_back(line);
+                        flag = false;
+                        break;
+                    }
+                }
+                // if closing comment symbol */ not at the end of line, but no more
+                // printable objects behind closing comment symbol, then convert
+                if (flag){
+                    line.replace(pos_first, 2, "//");
+                    line.replace(pos_second, 2, "");
+                    flag = false;
+                    out_file.push_back(line);
+                }
+            }
+        }
+        
+        // checking multi-line comments
+        else {
+            buffer.push_back(line);
+            // if only starting c-style comment symbol /*, append line to buffer and activate
+            // flag marking start of c-style comment
+            if (!flag && pos_first != -1){
+                flag = true;
+            }
+            // adding next comment line to buffer
+            else if (flag && pos_second == -1){
+                continue;
+            }
+            // checking closing line with */
+            else {
+                if (pos_second + 1 == line.size() - 1){
+                    for (int i = pos_second + 2; i < line.size(); i++){
+                        if (std::isprint(line[i])) {
+                            for (int j = 0; j < buffer.size(); j++){
+                                out_file.push_back(buffer[j]);
+                            }
+                            buffer.clear();
+                            flag = false;
+                            break;
+                        }                
+                    }
+                }
+                if (flag){
+                    for (int j = 0; j < buffer.size(); j++){
+                        if (j == 0){
+                            pos_first = buffer[j].find("/*");
+                            buffer[j].replace(pos_first, 2, "//");
+                        } else if (j == buffer.size() - 1){
+                            buffer[j].replace(pos_second, 2, "");
+                            buffer[j].insert(0, "// ");
+                        } else {
+                            buffer[j].insert(0, "// ");
+                        }
+                        out_file.push_back(buffer[j]);
+                        buffer.clear();
+                    }
+                }
+                flag = false;                
+            }
+        }
+    }
+    in_file.close();
+    
+    // copying converted data from vector to initial cpp script
+    std::ofstream output_file;
+    output_file.open(file_name);
+    for (auto i: out_file) 
+        output_file << i << std::endl;
+    output_file.close();
+    return 0;
+}
+
+int main() {
+    convert_comments("to_replace.cpp");
+    return 0;
+}
---
+++ interview_questions/interview_questions_codelite/task_2_refactoring/task_2_refactoring.mk
@@ -0,0 +1,108 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Debug
+ProjectName            :=task_2_refactoring
+ConfigurationName      :=Debug
+WorkspacePath          :=/home/devpyth/all/c++/Cpp_exercises_with_solutions/interview_questions/interview_questions_codelite
+ProjectPath            :=/home/devpyth/all/c++/Cpp_exercises_with_solutions/interview_questions/interview_questions_codelite/task_2_refactoring
+IntermediateDirectory  :=$(ConfigurationName)
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=devpyth
+Date                   :=26/06/20
+CodeLitePath           :=/home/devpyth/.codelite
+LinkerName             :=/usr/bin/g++
+SharedObjectLinkerName :=/usr/bin/g++ -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
+Preprocessors          :=
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="task_2_refactoring.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=mkdir -p
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+##
+AR       := /usr/bin/ar rcu
+CXX      := /usr/bin/g++
+CC       := /usr/bin/gcc
+CXXFLAGS :=  -g -O0 -Wall $(Preprocessors)
+CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := /usr/bin/as
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=/usr/share/codelite
+Objects0=$(IntermediateDirectory)/task_2_refactoring.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@test -d $(ConfigurationName) || $(MakeDirCommand) $(ConfigurationName)
+
+
+$(IntermediateDirectory)/.d:
+	@test -d $(ConfigurationName) || $(MakeDirCommand) $(ConfigurationName)
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/task_2_refactoring.cpp$(ObjectSuffix): task_2_refactoring.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/task_2_refactoring.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/task_2_refactoring.cpp$(DependSuffix) -MM task_2_refactoring.cpp
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/devpyth/all/c++/Cpp_exercises_with_solutions/interview_questions/interview_questions_codelite/task_2_refactoring/task_2_refactoring.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/task_2_refactoring.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/task_2_refactoring.cpp$(PreprocessSuffix): task_2_refactoring.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/task_2_refactoring.cpp$(PreprocessSuffix) task_2_refactoring.cpp
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) -r $(ConfigurationName)/
+
+
