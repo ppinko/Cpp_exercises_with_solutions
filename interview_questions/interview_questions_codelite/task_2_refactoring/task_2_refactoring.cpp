b6528af9003ad01ab4190633483e07e96ca5beb8
ppinko <p.pinkowicz@gmail.com>
2020-06-28 06:49:59 +0200

    Fully functional version of second task.

--- interview_questions/interview_questions_codelite/task_2_refactoring/task_2_refactoring.cpp
+++ interview_questions/interview_questions_codelite/task_2_refactoring/task_2_refactoring.cpp
@@ -7,12 +7,14 @@
 // function prototype
 int convert_comments(std::string file_name);
 std::vector<int> counting_comments(std::string line);
+std::string one_line_comments(std::string line, int pos_start, int pos_end);
 
 // function checking number of starting and closing comment symbol in one line
 std::vector<int> counting_comments(std::string line){
     std::vector<int> counter {0, 0};
     std::string start_comment {"/*"}, end_comment {"*/"};
     int i {0};
+    if (line.empty()) return counter;
     while (i < line.size() - 1){
         std::string temp = line.substr(i, 2);
         if (temp == start_comment || temp == end_comment){
@@ -22,6 +24,27 @@ std::vector<int> counting_comments(std::string line){
         } else i++;
     }
     return counter;
+}
+
+std::string one_line_comments(std::string line, int pos_start, int pos_end){
+            // if closing comment symbol */ at the end of line, then convert line
+            if (pos_end + 1 == line.size() - 1){
+                line.replace(pos_start, 2, "//");
+                line.replace(pos_end, 2, "");
+            } else {
+                // if closing comment symbol */ not at the end of line and printable
+                // objects behind closing comment symbol, then no conversion
+                for (int i = pos_end + 2; i < line.size(); i++){
+                    if (!std::isspace(line[i])) {
+                        return line;
+                    }
+                }
+                // if closing comment symbol */ not at the end of line, but no more
+                // printable objects behind closing comment symbol, then convert
+                line.replace(pos_start, 2, "//");
+                line.replace(pos_end, 2, "");
+            }
+            return line;
 }
 
 int convert_comments(std::string file_name){
@@ -34,7 +57,7 @@ int convert_comments(std::string file_name){
         return 1;
     }
     
-    // setting variables
+    // setting variabless
     std::vector<std::string> out_file {};
     std::vector<std::string> buffer {};
     std::string line{};
@@ -44,44 +67,23 @@ int convert_comments(std::string file_name){
     
     while (std::getline(in_file, line)){
         line_no++;
-        pos_start = line.find("/*");
-        pos_end = line.find("*/");
+        pos_start = line.rfind("/*");
+        pos_end = line.rfind("*/");
+//        std::cout << line << std::endl;
+//        std::cout << line_no << "=" << pos_start << "=" << pos_end << std::endl;
+        std::vector<int> counter = counting_comments(line);
+//        std::cout << counter[0] << " & " << counter[1] << std::endl;
     
         // lack of starting c-style comment symbol /* and flag equals false, copy original line
         if (!flag && pos_start == -1) 
             out_file.push_back(line);
         
         // checking one-line comments
-        else if (!flag && pos_start != -1 && pos_end != -1){
-            std::vector<int> counter = counting_comments(line);
-            std::cout << counter[0] << " & " << counter[1] << std::endl;
-            // if closing comment symbol */ at the end of line, then convert line
-            if (pos_end + 1 == line.size() - 1){
-                line.replace(pos_start, 2, "//");
-                line.replace(pos_end, 2, "");
-                out_file.push_back(line);
-            } else {
-                // if closing comment symbol */ not at the end of line and printable
-                // objects behind closing comment symbol, then no conversion
-                flag = true;
-                for (int i = pos_end + 2; i < line.size(); i++){
-                    if (std::isprint(line[i])) {
-                        out_file.push_back(line);
-                        flag = false;
-                        break;
-                    }
-                }
-                // if closing comment symbol */ not at the end of line, but no more
-                // printable objects behind closing comment symbol, then convert
-                if (flag){
-                    line.replace(pos_start, 2, "//");
-                    line.replace(pos_end, 2, "");
-                    flag = false;
-                    out_file.push_back(line);
-                }
-            }
+        else if (!flag && pos_start != -1 && pos_end != -1 && pos_end > pos_start){
+            std::string one_line = one_line_comments(line, pos_start, pos_end);
+            std::cout << "ONE-LINER = " << one_line << std::endl;
+            out_file.push_back(one_line);
         }
-        
         // checking multi-line comments
         else {
             buffer.push_back(line);
@@ -95,6 +97,16 @@ int convert_comments(std::string file_name){
             else if (flag && pos_end == -1){
                 std::cout << "Second: " << line_no << std::endl;
                 continue;
+            }
+            else if (flag && pos_start != -1){
+                for (int j = 0; j < buffer.size() - 1; j++){
+                    out_file.push_back(buffer[j]);
+                }
+                buffer.clear();
+                if (pos_end > pos_start){
+                    std::string complex_line = one_line_comments(line, pos_start, pos_end);
+                    out_file.push_back(complex_line);
+                } else buffer.push_back(line);
             }
             // checking closing line with */
             else {
@@ -116,7 +128,7 @@ int convert_comments(std::string file_name){
                     std::cout << "Fourth: " << line_no << std::endl;
                     for (int j = 0; j < buffer.size(); j++){
                         if (j == 0){
-                            pos_start = buffer[j].find("/*");
+                            pos_start = buffer[j].rfind("/*");
                             buffer[j].replace(pos_start, 2, "//");
                         } else if (j == buffer.size() - 1){
                             buffer[j].replace(pos_end, 2, "");
@@ -137,8 +149,10 @@ int convert_comments(std::string file_name){
     // copying converted data from vector to initial cpp script
     std::ofstream output_file;
     output_file.open(file_name);
-    for (auto i: out_file) 
+    for (auto i: out_file){
         output_file << i << std::endl;
+        std::cout << i << std::endl;
+    }
     output_file.close();
     return 0;
 }
