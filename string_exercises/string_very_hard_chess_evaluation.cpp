22d345059e632756b45f2350e09cfb65576baa39
ppinko <p.pinkowicz@gmail.com>
2020-07-03 19:08:29 +0200

    Adding hard string exercise - chess move evaluation, part 2 - bishop and rock.

--- string_exercises/string_very_hard_chess_evaluation.cpp
+++ string_exercises/string_very_hard_chess_evaluation.cpp
@@ -7,18 +7,25 @@
 #include <cmath>
 
 bool canMove(std::string piece, std::string current, std::string target) {
+    int a = std::abs(int(target.at(0)) - int(current.at(0)));
+    int b = std::abs(int(target.at(1)) - int(current.at(1)));
+
 	if (piece == "Pawn"){
         if (current.at(0) == target.at(0) && ((int(target.at(1)) - int(current.at(1)) == 1 
             || target.at(1) == '4' && current.at(1) == '2')))
             return true;
-        else return false;
     }
     else if (piece == "Knight"){
-        int a = std::abs(int(target.at(0)) - int(current.at(0)));
-        int b = std::abs(int(target.at(1)) - int(current.at(1)));
         if (std::fmin(a, b) == 1 && std::fmax(a, b) == 2)
             return true;
-        else return false;
+    }
+    else if (piece == "Bishop"){
+        if (a == b)
+            return true;
+    }
+    else if (piece == "Rook"){
+        if (current.at(0) == target.at(0) || current.at(1) == target.at(1))
+            return true;
     }
     return false;
 }
@@ -31,5 +38,10 @@ int main(){
 	std::cout << canMove("Pawn", "C6", "D7") << std::endl;
     std::cout << canMove("Knight", "F5", "E3") << std::endl;
 	std::cout << canMove("Knight", "F6", "E5") << std::endl;
+    std::cout << canMove("Bishop", "B4", "E7") << std::endl;
+	std::cout << canMove("Bishop", "B6", "F5") << std::endl;
+    std::cout << canMove("Rook", "A8", "H8") << std::endl;
+	std::cout << canMove("Rook", "A8", "H7") << std::endl;
+    
     return 0;
 }
