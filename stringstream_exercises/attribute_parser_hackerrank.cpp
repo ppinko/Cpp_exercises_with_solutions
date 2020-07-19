52621029e22d41c08078a96ca47f6c5dd5fc65aa
ppinko <p.pinkowicz@gmail.com>
2020-07-19 14:18:06 +0200

    Adding first partially functioning solution.

--- stringstream_exercises/attribute_parser_hackerrank.cpp
+++ stringstream_exercises/attribute_parser_hackerrank.cpp
@@ -26,10 +26,12 @@ int main() {
     std::string temp;
     std::string tag_name;
     std::string tag_attr;
+    std::string tag_val;
     std::string opening_tag {};
     std::string tag_key;
 
     std::map<std::string, std::string> m {};
+
     for (int i = 0; i <= row; ++i)
     {   
         std::getline(std::cin, temp);
@@ -38,15 +40,41 @@ int main() {
             int start_tag_pos = temp.find("<tag");
             int end_tag_pos = temp.find(" ", start_tag_pos);
             tag_name = temp.substr(start_tag_pos +1, end_tag_pos - start_tag_pos - 1);
-            std::cout << tag_name << std::endl;
             int tag_attr_end = temp.find(" ", end_tag_pos + 1);
             tag_attr = temp.substr(end_tag_pos+1, tag_attr_end-end_tag_pos-1);
             if (opening_tag.size() == 0){
                 tag_key = tag_name + "~" + tag_attr;
+                opening_tag = tag_name;
             }
-            std::cout << tag_key << std::endl;
+            else {
+                tag_key = opening_tag + "." + tag_name + "~" + tag_attr;
+                opening_tag += "." + tag_name;
+            }
+            int tag_val_start = temp.find('"', tag_attr_end) + 1;
+            int tag_val_end = temp.find('"', tag_val_start) - 1;
+            tag_val = temp.substr(tag_val_start, tag_val_end-tag_val_start+1);
+            m[tag_key] = tag_val;
         }
-        
+        if (temp.find("</tag") != std::string::npos)
+        {
+            if (opening_tag.rfind(".") == std::string::npos)
+                opening_tag.clear();
+            else {
+                int last_tag_pos = opening_tag.rfind(".");
+                opening_tag = opening_tag.substr(0, last_tag_pos);
+            }
+        }
+    }
+
+    for (int i = 0; i < queries; ++i){
+        std::string q_key;
+        std::cin >> q_key;
+        auto it = m.find(q_key);
+        if (it != m.end())
+            std::cout << m[q_key] << std::endl;
+        else {
+            std::cout << "Not Found!" << std::endl;
+        }
     }
 
 
