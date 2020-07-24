ee5ff5b0e7bc4c7aeffef7d5f6e68ef52d8b1f0a
ppinko <p.pinkowicz@gmail.com>
2020-07-24 20:44:21 +0200

    Adding math exercise (a+b)^n formula.

--- math_exercises/abn_formula.cpp
+++ math_exercises/abn_formula.cpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <iterator>
 #include <iostream>
-#include <cmath>
 #include <vector>
 #include <algorithm>
 
@@ -39,13 +38,60 @@ std::vector<int> binomialCoeff(int n)
 } 
 
 std::string Formula(int n) {
-    return "yes";
+    int n_pos = std::abs(n);
+    std::vector<int> binomial_coefficients = binomialCoeff(n_pos);
+    std::string ans {};
+    if (n == 0)
+        return "1";
+    
+    int a_pow, b_pow, temp;
+    for (int i = 0; i < binomial_coefficients.size(); ++i)
+    {
+        a_pow = n_pos - i;
+        b_pow = i;
+        temp = binomial_coefficients[i];
+        if (temp != 1)
+        {
+        ans.append(std::to_string(temp));
+        }
+        if (a_pow == 1)
+        {
+            ans.push_back('a');
+        }
+        else if (a_pow > 1)
+        {
+            ans.append("a^");
+            ans.append(std::to_string(a_pow));
+        }
+
+        if (b_pow == 1)
+        {
+            ans.push_back('b');
+        }
+        else if (b_pow > 1)
+        {
+            ans.append("b^");
+            ans.append(std::to_string(b_pow));
+        }
+        ans.push_back('+');
+    }
+    ans.erase(ans.size() - 1);
+    
+    if (n < 0)
+    {   
+        ans = "1/(" + ans + ")";
+    }
+
+    return ans;
 }
 
 int main(){
 
-    auto vec = binomialCoeff(1);
-    std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
+    // auto vec = binomialCoeff(4);
+    // std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
+
+    std::string test_1 = Formula(-2);
+    std::cout << test_1 << std::endl;
 
     return 0;
 }
