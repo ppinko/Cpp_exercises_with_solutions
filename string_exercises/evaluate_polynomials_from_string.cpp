1cbefdd09971dd94c87a10ec9b4fc25a1391e76e
ppinko <p.pinkowicz@gmail.com>
2020-07-28 22:51:30 +0200

    Developing evaluate polynomials from string exercise - refactoring main function.

--- string_exercises/evaluate_polynomials_from_string.cpp
+++ string_exercises/evaluate_polynomials_from_string.cpp
@@ -10,7 +10,7 @@
 #include <set>
 
 bool evaluate_brackets(const std::string &str);
-void adding_multiply_sign(std::string &str);
+bool adding_multiply_sign(std::string &str);
 void multiply_sign_in_front_of_x(std::string &str);
 int evalPolynomial(std::string poly, int num); 
 bool testing_operator_correctness(const std::string &str);
@@ -63,7 +63,7 @@ bool testing_operator_correctness(const std::string &str){
     return true;
 }
 
-void adding_multiply_sign(std::string &str)
+bool adding_multiply_sign(std::string &str)
 {
     auto it = std::find(str.begin(), str.end(), '(');
     while (it != str.end())
@@ -79,9 +79,12 @@ void adding_multiply_sign(std::string &str)
                 str.insert(it+1, '*');
                 it += 3;
             }
+            else
+                return false;
         }
         it = std::find(it, str.end(), '(');
     }
+    return true;
 }
 
 void multiply_sign_in_front_of_x(std::string &str)
@@ -120,52 +123,57 @@ int evalPolynomial(std::string poly, int num) {
     poly.erase(std::remove(poly.begin(), poly.end(), ' '), poly.end());
     if (poly.size() == 0 || !evaluate_brackets(poly))
         return -1;
-    if (poly.find('(') != std::string::npos)
-        adding_multiply_sign(poly);
+    if (poly.find('(') != std::string::npos){
+        bool test = adding_multiply_sign(poly);
+        if (!test)
+            return -1;
+    }
     if (poly.find('x') != std::string::npos)
     {
         multiply_sign_in_front_of_x(poly);
         replace_x(poly, num);
     }
+    if (!testing_operator_correctness(poly))
+        return -1;
     return 0;
 
 }
 
 int main()
 {
-    // Testing adding_multiply_sign function
-    std::string str {"2(x+2)+x(x-1)"};
-    adding_multiply_sign(str);
-    assert((str == "2*(x+2)+x*(x-1)"));
+    // // Testing adding_multiply_sign function
+    // std::string str {"2(x+2)+x(x-1)"};
+    // adding_multiply_sign(str);
+    // assert((str == "2*(x+2)+x*(x-1)"));
 
-    // Testing multiply_sign_in_front_of_x function
-    std::string str_2 {"3x^2/8"};
-    multiply_sign_in_front_of_x(str_2);
-    assert((str_2 == "3*x^2/8"));
+    // // Testing multiply_sign_in_front_of_x function
+    // std::string str_2 {"3x^2/8"};
+    // multiply_sign_in_front_of_x(str_2);
+    // assert((str_2 == "3*x^2/8"));
 
-    // Testing multiply_sign_in_front_of_x function
-    std::string str_3 {"3x&2/8"};
-    assert((testing_operator_correctness(str_3) == false));
+    // // Testing multiply_sign_in_front_of_x function
+    // std::string str_3 {"3x&2/8"};
+    // assert((testing_operator_correctness(str_3) == false));
 
-    std::string str_4 {"5//2"};
-    assert((testing_operator_correctness(str_4) == false));
+    // std::string str_4 {"5//2"};
+    // assert((testing_operator_correctness(str_4) == false));
 
-    std::string str_5 {"5/2*2*(2+65)"};
-    assert((testing_operator_correctness(str_5) == true));
+    // std::string str_5 {"5/2*2*(2+65)"};
+    // assert((testing_operator_correctness(str_5) == true));
 
-    // Testing replace_x function
-    std::string str_6 {"2(x+2)+x(x-1)"};
-    replace_x(str_6, 10);
-    assert((str_6 == "2(10+2)+10(10-1)"));
+    // // Testing replace_x function
+    // std::string str_6 {"2(x+2)+x(x-1)"};
+    // replace_x(str_6, 10);
+    // assert((str_6 == "2(10+2)+10(10-1)"));
 
     assert((evalPolynomial("4(x + 3))/2", 5) == -1)); // Invalid because parentheses not balanced.
     // assert((evalPolynomial("x+1", 1) == 2));
 	// assert((evalPolynomial("x^2", 2) == 4)); // Check exponentation.
 	// assert((evalPolynomial("2(x+2)+x(x-1)", 3) == 16)); // Check multiplication.
 	// assert((evalPolynomial("3x^2/8", 4) == 6));
-	// assert((evalPolynomial("3x&2/8", 5) == -1)); // & not a valid mathematical expression.
-	// assert((evalPolynomial("print(x)", 6) == -1)); // print(x) not a valid mathematical expression.
-	// assert((evalPolynomial("x//2", 7) == -1)); // // not a valid operator.
+    assert((evalPolynomial("3x&2/8", 5) == -1)); // & not a valid mathematical expression.
+    assert((evalPolynomial("print(x)", 6) == -1)); // print(x) not a valid mathematical expression.
+	assert((evalPolynomial("x//2", 7) == -1)); // // not a valid operator.
 	assert((evalPolynomial("", 8) == -1)); // Expression empty.
 
     std::cout << "Success" << std::endl;
