e3f934d459fa4a8932eb0c51ac1477c8f1d9f399
ppinko <p.pinkowicz@gmail.com>
2020-08-05 21:48:01 +0200

    Adding string exercise - ice cream sandwich.

---
+++ string_exercises/ice_cream_sandwich.cpp
@@ -0,0 +1,51 @@
+// https://edabit.com/challenge/GHaTpruxeoNc77Lto
+
+#include <iostream>
+#include <cassert>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <set>
+
+using namespace std;
+
+bool isIcecreamSandwich(string str) {
+    set<char> letters {};
+    for (const auto &c : str)
+        letters.insert(c);
+
+    if (letters.size() != 2)
+        return false;
+    
+    int str_length = str.size();
+    for (int i = 0; i < str_length; ++i)
+    {
+        if (str.at(i) != str.at(str_length - i - 1))
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+
+    assert((isIcecreamSandwich("AABBBAA") == true));
+    assert((isIcecreamSandwich("3&&3") == true));
+    assert((isIcecreamSandwich("yyyyymmmmmmmmyyyyy") == true));
+    assert((isIcecreamSandwich("hhhhhhhhmhhhhhhhh") == true));
+    assert((isIcecreamSandwich("CDC") == true));
+
+    assert((isIcecreamSandwich("BBBBB") == false));
+
+    assert((isIcecreamSandwich("AAACCCAA") == false));
+
+    assert((isIcecreamSandwich("AACDCAA") == false));
+
+    assert((isIcecreamSandwich("AAABB") == false));
+    assert((isIcecreamSandwich("AA") == false));
+    assert((isIcecreamSandwich("A") == false));
+    assert((isIcecreamSandwich("") == false));
+
+    return 0;
+}
