6746af02a31be61cf892d4017fd63ee029cac71e
ppinko <p.pinkowicz@gmail.com>
2020-10-03 20:41:53 +0200

    Adding string exercise - needl in the hex.

---
+++ string_exercises/needle_in_hex_string.cpp
@@ -0,0 +1,28 @@
+// https://edabit.com/challenge/sAkNHEKK8wu4fnyYL
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int firstIndex(const string &str_hex, const string &needle) {
+    istringstream oss {str_hex};
+    string temp {}, translated {};
+    while (oss >> temp){
+        stringstream ss;
+        int x;
+        ss << std::hex << temp;
+        ss >> x;
+        translated += (char)x;
+    }
+    return translated.find(needle);
+}
+
+
+int main(int argc, char *argv[]){
+
+    cout << firstIndex("68 65 6c 6c 6f 20 77 6f 72 6c 64", "world") << endl;
+
+    return 0;
+}
